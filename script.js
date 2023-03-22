bf3a309595ed3dbcf796ece1a0dfb7b819de8b67
Maria Vitória <mariavitoiafavero@gmail.com>
2023-03-22 20:54:25 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMode() {
+    const html = document.documentElement
+    if(html.classList.contains("light")){
+        html.classList.remove("light")
+    } else {
+        html.classList.add("light")
+    }
+    /* html.classList.toggle("light")*/
+    const img = document.querySelector("#profile img")
+    if(html.classList.contains("light")){
+       img.setAttribute("src", "./assets/Avatar.png")
+    } else {
+        img.setAttribute("src", "./assets/Avatar1.png")
+    }
+}
